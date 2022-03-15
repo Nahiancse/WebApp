c3aff91ff0a70229d319ee61e98ded3a2b8796c2
cnahian4@gmail.com <cnahian4@gmail.com>
2022-03-15 10:11:56 +0600

    Add project files.

---
+++ WebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApp.ViewModel;
+
+namespace WebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IConfiguration _Configure;
+        private readonly string apiBaseUrl;
+        public EmployeeController(IConfiguration configuration)
+        {
+            _Configure = configuration;
+            apiBaseUrl = _Configure.GetValue<string>("WebAPIBaseUrl");
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<EmployeeVM> reservationList = new List<EmployeeVM>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:5001/api/Employee"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationList = JsonConvert.DeserializeObject<List<EmployeeVM>>(apiResponse);
+                }
+            }
+            return View(reservationList);
+        }
+    }
+}
